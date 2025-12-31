41416dc7459421a18b14731fc99f8e07d45182c5
danieleciciani <daniele.ciciani96@gmail.com>
2025-12-31 15:38:16 +0100

    added TriggerDagOperator to main dag script

--- dags/main.py
+++ dags/main.py
@@ -30,27 +30,33 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
+
+
 #dag 1: produce_json
-with DAG (
-    dag_id = 'produce_json',
-    default_args = default_args,
-    description = 'Dag to produce Json file with Raw data',
-    schedule='0 14 * * *',
-    catchup = False,
-) as dag:
+with DAG(
+    dag_id="produce_json",
+    default_args=default_args,
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False,
+) as dag_produce:
 
     # Define tasks
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    extracted_data = extract_video_data(video_ids)
-    save_to_json_task = save_to_json(extracted_data)
+    extract_data = extract_video_data(video_ids)
+    save_to_json_task = save_to_json(extract_data)
+
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
 
     # Define dependencies
-    playlist_id >> video_ids >> extracted_data >> save_to_json_task
-
-# Variables
-staging_schema = "staging"
-core_schema = "core"
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 #dag 2: update_db
 with DAG(
@@ -65,8 +71,14 @@ with DAG(
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # Define dependencies
-    update_staging >> update_core 
+    update_staging >> update_core >> trigger_data_quality
+
 
 # DAG 3: data_quality
 with DAG(
